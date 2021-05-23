6ebbb31f5e6b266e77746f17ec8eb3618219c514
Jakub Meysner <hello@jakubmeysner.me>
2021-05-23 22:39:21 +0200

    feat: add separate icon for paywalled features to overlay comparison

--- pages/index.vue
+++ pages/index.vue
@@ -155,6 +155,22 @@
                   </svg>
 
                   <svg
+                    v-else-if="alternative.available === 'PAYWALL'"
+                    class="text-purple-400 h-8 w-8"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+
+                  <svg
                     v-else-if="alternative.available === 'UNKNOWN'"
                     class="text-gray-400 h-8 w-8"
                     fill="none"
@@ -266,9 +282,7 @@ export default Vue.extend({
                 available: 'YES',
               },
               {
-                available: 'PARTIAL',
-                comment:
-                  "Requires payment. It's independence has been questioned.",
+                available: 'PAYWALL',
               },
               {
                 available: 'NO',
@@ -319,8 +333,7 @@ export default Vue.extend({
                 available: 'YES',
               },
               {
-                available: 'PARTIAL',
-                comment: 'Requires payment.',
+                available: 'PAYWALL',
               },
               {
                 available: 'YES',
@@ -365,6 +378,23 @@ export default Vue.extend({
             ],
           },
           {
+            name: 'Custom sort',
+            alternatives: [
+              {
+                available: 'YES',
+              },
+              {
+                available: 'PAYWALL',
+              },
+              {
+                available: 'YES',
+              },
+              {
+                available: 'NO',
+              },
+            ],
+          },
+          {
             name: 'Fullscreen support',
             alternatives: [
               {
@@ -372,8 +402,7 @@ export default Vue.extend({
                 comment: 'Available with free 3rd party app.',
               },
               {
-                available: 'PARTIAL',
-                comment: 'Requires payment.',
+                available: 'PAYWALL',
               },
               {
                 available: 'YES',
